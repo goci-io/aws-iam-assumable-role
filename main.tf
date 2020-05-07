88b43db750635e7e28e14d6d99d1007340421faa
etwillbefine <tim.gatzemeier@gmx.de>
2020-05-07 12:00:02 +0200

    initial code commit

---
+++ main.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_version = ">= 0.12.1"
+
+  required_providers {
+    aws    = "~> 2.50"
+    random = "~> 2.2"
+  }
+}
+
+module "label" {
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  namespace   = var.namespace
+  stage       = var.stage
+  environment = var.environment
+  attributes  = var.attributes
+  delimiter   = var.delimiter
+  tags        = var.tags 
+}
+
+data "aws_iam_policy_document" "trust_relation" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    
+    dynamic "principals" {
+      for_each = length(var.trusted_iam_arns) > 0 ? [1] : []
+
+      content {
+        type        = "AWS"
+        identifiers = var.trusted_iam_arns
+      }
+    }
+
+    dynamic "principals" {
+      for_each = length(var.trusted_services) > 0 ? [1] : []
+
+      content {
+        type        = "Service"
+        identifiers = var.trusted_services
+      }
+    }
+  }
+}
+
+data "aws_iam_policy_document" "permissions" {
+  dynamic "statement" {
+    for_each = var.policy_statements
+
+    content {
+      effect    = lookup(statement.value, "effect", "Allow")
+      actions   = lookup(statement.value, "actions")
+      resources = lookup(statement.value, "resources")
+    }
+  }
+}
+
+resource "aws_iam_role" "role" {
+  count              = var.enabled ? 1 : 0
+  name               = module.label.id
+  tags               = module.label.tags
+  assume_role_policy = data.aws_iam_policy_document.trust_relation.json
+}
+
+resource "aws_iam_role_policy" "policy" {
+  count  = var.enabled ? 1 : 0
+  name   = module.label.id
+  role   = join("", aws_iam_role.role.*.id)
+  policy = data.aws_iam_policy_document.permissions.json
+}
+
+resource "aws_iam_role_policy_attachment" "attachments" {
+  count      = var.enabled ? length(var.policy_attachments) : 0
+  role       = join("", aws_iam_role.role.*.id)
+  policy_arn = element(var.policy_attachments, count.index)
+}
---
+++ variables.tf
@@ -0,0 +1,83 @@
+variable "namespace" {
+  type        = string
+  default     = ""
+  description = "Namespace, which could be your organization name or abbreviation, e.g. 'goci' or 'cp'"
+}
+
+variable "environment" {
+  type        = string
+  default     = ""
+  description = "Environment, e.g. 'prod', 'staging', 'dev', 'pre-prod', 'UAT'"
+}
+
+variable "stage" {
+  type        = string
+  default     = ""
+  description = "Stage, e.g. 'prod', 'staging', 'dev', OR 'source', 'build', 'test', 'deploy', 'release'"
+}
+
+variable "name" {
+  type        = string
+  default     = ""
+  description = "Solution name, e.g. 'app' or 'jenkins'"
+}
+
+variable "enabled" {
+  type        = bool
+  default     = true
+  description = "Set to false to prevent the module from creating any resources"
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `environment`, `stage`, `name` and `attributes`"
+}
+
+variable "attributes" {
+  type        = list(string)
+  default     = []
+  description = "Additional attributes (e.g. `1`)"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags (e.g. `map('BusinessUnit','XYZ')`"
+}
+
+variable "with_external_id" {
+  type        = bool
+  default     = true
+  description = "Creates a random UUID as external ID and attaches an additional condition to the TrustRelation policy"
+}
+
+variable "rotate_external_id" {
+  type        = bool
+  default     = true
+  description = "Enables a kepper on the random UUID to rotate the value every month (pipeline must run)."
+}
+
+variable "trusted_iam_arns" {
+  type        = list(string)
+  default     = []
+  description = "IAM ARNs to allow sts:AssumeRole on the created role. For eample an IAM or Account ARN"
+}
+
+variable "trusted_services" {
+  type        = list(string)
+  default     = []
+  description = "Additional trusted AWS services like ec2.amazonaws.com to be allowed to assume the role"
+}
+
+variable "policy_attachments" {
+  type        = list(string)
+  default     = []
+  description = "Additional existing policy ARNs to attach to the role"
+}
+
+variable "policy_statements" {
+  type        = list(any)
+  default     = []
+  description = "Policy attachments in Terraform style (effect, actions, resources)"
+}
